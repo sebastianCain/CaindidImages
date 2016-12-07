cdeda69c798047b665b15784da58b2e79a169c4e
Yuxuan <ychen14@stuy.edu>
2016-12-07 14:06:06 -0500

    Testing how to upload image using an image link, still researching

--- uploadTest.py
+++ uploadTest.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, request, redirect, url_for#, send_from_directory <-- Unnecessary?
 from werkzeug.utils import secure_filename
-
+import urllib.request
 
 app = Flask(__name__)
 path="data"
@@ -19,35 +19,25 @@ def checkFile(filename):
 @app.route("/", methods = ['GET', 'POST'])
 def upload():
     if request.method == 'POST':
-        '''#check if there is actually file uploaded by user
-        if 'file' not in request.files:
-            flash("No file")
-            return redirect(request.url)
-
-        ^the lines of code below already cover this'''
-        
         file = request.files['file']
-
-        #check if filename is empty
-        '''if file.filename == '':
-            flash("No selected file")
-            return redirect(request.url)
-
-        ^I lied, i think browsers do this for us, we dont really need it'''
-        
-        #if file is uploaded and filename is valid, save to the specified path (directory)
-        if file and checkFile(file.filename):
+        if file.filename == "":
+            print "LINK"
+            print request.form['link']
+            urlretrieve(request.form['link'],"test.jpg")
+    #if file is uploaded and filename is valid, save to the specified path (directory)
+        elif file and checkFile(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(path,filename))
             return "Image uploaded."
     return '''
-    <!DOCTYPE html>
-    <title>Upload New File</title>
-    <form method='POST' action="" enctype="multipart/form-data">
-      <input type="file" name="file">
-      <input type="submit" value="Upload">
-    </form>
-    '''
+<!DOCTYPE html>
+<title>Upload New File</title>
+<form method='POST' action="" enctype="multipart/form-data">
+<input type="file" name="file">
+<input type="text" name="link">
+<input type="submit" value="Upload">
+</form>
+'''
 
 if __name__ == "__main__":
     app.debug = True
