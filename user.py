06cdd87923243d0b9f41d1996fa0de185102db64
JiaqiGao <jiaqigao12@gmail.com>
2016-12-09 01:24:35 -0500

    scrambled eggs

--- user.py
+++ user.py
@@ -38,26 +38,6 @@ def get_UID(username):
     c.execute("SELECT id FROM users WHERE username = '" + username + "'")
     return c.fetchone()[0]
 
-def get_stories(uid):
-    db = sqlite3.connect(DATABASE)
-    c = db.cursor()
-
-    query = "SELECT DISTINCT stories.storyid,stories.title from updates,stories WHERE userid = ? AND updates.storyid = stories.storyid"
-    c.execute(query, (uid,))
-
-    result = c.fetchall()
-    db.close()
-    return result if result else []
-
-def get_info(uid):
-    results = get_user(id=uid)
-    info = {
-        'uid' : results [0],
-        'username' : results[1],
-      
-        }
-    return info
-
 #For now, only work with parameters path and uid
 def add_pic(path,uid):
     db = sqlite3.connect(DATABASE)
