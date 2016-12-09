137e8623206574177efae8db3a3f554a98a361e6
Lawrence Liu <lawrence255@yahoo.com>
2016-12-09 14:04:50 -0500

    cleanup

--- clarifaiTest.py
+++ clarifaiTest.py
@@ -1,13 +1,3 @@
-#Adding all the potentially useful clarifai tag request stuffs
-
-'''curl "https://api.clarifai.com/v1/tag/?url=https://samples.clarifai.com/metro-north.jpg"
-  -H "Authorization: Bearer {UjnFoZqHIByaxZDPdXI1yCrnIZG0Nd}"
-
-
-curl "https://api.clarifai.com/v1/tag/" \
-  -X POST -F "encoded_data=@/Users/USER/my_image.jpeg" \
-  -H "Authorization: Bearer {UjnFoZqHIByaxZDPdXI1yCrnIZG0Nd}"'''
-
 from flask import Flask, render_template
 import urllib2, json
 
@@ -24,7 +14,3 @@ def root():
 if __name__ == "__main__":
     app.debug = True
     app.run()
-
-
-#output stored in 'results' key as an array
-#response[results[tag[classes]]]
