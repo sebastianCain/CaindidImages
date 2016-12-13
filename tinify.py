5ed68af4fb0df882970b6af01cff735cd0922c7a
Lawrence Liu <lawrence255@yahoo.com>
2016-12-12 20:54:55 -0500

    tinify api, just need a double check on the url and it should be able to integrate into app.py smoothly

---
+++ tinify.py
@@ -0,0 +1,9 @@
+import urllib2, json
+
+#returns the url of the compressed image from tinypng
+def uploadPic (url, filename):
+    u = urllib2.urlopen("https://api.tinify.com/shrink?user_api:N5bZsPRvTbuuTmdrXykaLC7WJPmnrW3N&data-binary:@" + filename + "&dump-header:" + url)
+    response = u.read()
+    data = json.loads(response)
+    return data["source"]["url"]
+#Not sure that's the right link, refer to https://tinypng.com/developers/reference
